499bcd6c0bb978ba3518b37484ccb9821a8478bc
Tysterman <tynutsathitya@gmail.com>
2019-12-03 19:46:18 -0800

    Initial commit of project

---
+++ Sonder/Assets/AptBuildingLightScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AptBuildingLightScript : MonoBehaviour
+{
+    private List<GameObject> lights;
+    public float secondsUntilLightTrigger = 50.0f;
+
+    private float timer = 0.0f;
+    private int lastLightIndex = -1;
+    public int maxLightOnCount = 5;
+    private int currentLightOnCount = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = secondsUntilLightTrigger;
+        lights = new List<GameObject>();
+        foreach (Transform child in transform)
+        {
+            if (child.tag == "BuildingLight")
+            {
+                lights.Add(child.gameObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0.0f)
+        {
+            int lightIndex = Random.Range(0, lights.Count - 1);
+            GameObject light = lights[lightIndex];
+            bool active = light.activeSelf;
+
+            if (lightIndex != lastLightIndex)
+            {
+                if (currentLightOnCount <= maxLightOnCount && !active)
+                {
+                    light.SetActive(true);
+                    currentLightOnCount++;
+                    lastLightIndex = lightIndex;
+                }
+                else if (active)
+                {
+                    light.SetActive(false);
+                    currentLightOnCount--;
+                    lastLightIndex = lightIndex;
+                }
+            }
+            timer = secondsUntilLightTrigger;
+        }
+    }
+}
---
+++ Sonder/Assets/CarSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarSpawner : MonoBehaviour
+{
+    public GameObject carSpawn;
+
+    public CarSpawner otherSpawner;
+
+    public bool hasSpawned = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void spawnCar(GameObject car, Quaternion rotation)
+    {
+        print(carSpawn.transform);
+        //Instantiate(car, carSpawn.transform, true);
+        Instantiate(car, carSpawn.transform.position, rotation);
+        hasSpawned = true;
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.tag == "SpawnedCar")
+        {
+            Destroy(collision.gameObject);
+            otherSpawner.hasSpawned = false;
+        }
+    }
+}
